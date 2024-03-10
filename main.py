cd47ef4ac9586f3212eb78b6579b04a441da582e
Enzo Benko <[enzobenko@gmail.com]>
2024-03-10 13:35:40 -0300

    v.0.2

--- main.py
+++ main.py
@@ -2,27 +2,27 @@
 from functions import *
 import time
 
-'''
+
 #Coleta de dados
 t_start = time.perf_counter()
 get_data_market()
+selic_return()
+ibov_variance()
 get_data_stocks()
 get_data_funds()
+
+#Cálculo de indicadores
+#rodar retorno() primeiro
+#rodar sharpe antes dos outros indicadores
+
+for stock in Ibovespa_list:
+    retorno(stock)
+    volatilidade(stock)
+    sharpe(stock)
+    #treynor(data)
+    info_ratio(stock)
+    #sortino(data)
 t_stop = time.perf_counter()
 print(t_stop-t_start, 'segundos')
 
-#gets: 137.55519150000327 segundos
-'''
-
-#Processar Tabela Selic (Fonte: https://www3.bcb.gov.br/sgspub/consultarvalores/telaCvsSelecionarSeries.paint)
-#functions.daily_selic()
-
-
-#Cálculo de indicadores
-for stock in Ibovespa_list:
-    print(stock)
-    volatilidade(stock)
-    #sharpe(stock)
-    #treynor(data)
-    #info_ratio(data)
-    #sortino(data)
+#gets+std+return+sharpe: 501.6275638999996 segundos
